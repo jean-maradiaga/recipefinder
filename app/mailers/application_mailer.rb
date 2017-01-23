36d6fa457db983e542f889132d32f261a83e6d6d
Jean Maradiaga <jeanmaradiagac@gmail.com>
2017-01-23 01:43:38 -0600

    Added a custom email for user activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
+  add_template_helper(EmailHelper)
+
   default from: "noreply@recipefinder.com"
   layout 'mailer'
+  default 'content-type' => 'text/html'
 end
