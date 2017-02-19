f1c04c6674a7ba4417ad6e016a7dc07afc707486
Jean Maradiaga <jeanmaradiagac@gmail.com>
2017-02-19 16:59:39 -0600

    Added gzip compression

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Recipefinder
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
