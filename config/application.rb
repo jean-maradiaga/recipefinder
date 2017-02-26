28f9d0f3307147056ecdf7b2084ecda8217ee3d0
Jean Maradiaga <jeanmaradiagac@gmail.com>
2017-02-26 17:45:13 -0600

    Password recovery

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Recipefinder
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
